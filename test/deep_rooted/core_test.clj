9df924c4db87a2307036e667e7b949d463c8be41
Akshay Gupta <akshaygrkesh@gmail.com>
2022-01-22 10:38:45 +0530

    Initial Commit

---
+++ test/deep_rooted/core_test.clj
@@ -0,0 +1,7 @@
+(ns deep-rooted.core-test
+  (:require [clojure.test :refer :all]
+            [deep-rooted.core :refer :all]))
+
+(deftest a-test
+  (testing "Placeholder test."
+    (is (= 1 1))))
